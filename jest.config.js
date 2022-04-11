011cef551f7736cca4b8f2ae56c310f8413126f4
Bryan Lee <bryanmylee@gmail.com>
2022-04-12 01:40:54 +0800

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	transform: {
+		'^.+\\.svelte$': [
+			'svelte-jester',
+			{
+				preprocess: true
+			}
+		],
+		'^.+\\.ts$': 'ts-jest'
+	},
+	moduleFileExtensions: ['ts', 'svelte']
+};
