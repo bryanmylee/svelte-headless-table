66ba423845e3742352185d56c6442df17f3e188f
Bryan Lee <bryanmylee@gmail.com>
2022-04-13 19:27:34 +0800

    test: footer prototype

--- src/lib/utils/getFooterRows.test.ts
+++ src/lib/utils/getFooterRows.test.ts
@@ -5,418 +5,483 @@ import { createColumns, createDataColumn, createGroup } from './createColumns';
 import { getFooterRows } from './getFooterRows';
 
 describe('getFooterRows', () => {
-	test('no footers', () => {
-		const columns = createColumns<SampleRow>([
-			createDataColumn({
-				header: 'First Name',
-				key: 'firstName',
-			}),
-			createDataColumn({
-				header: 'Last Name',
-				key: 'lastName',
-			}),
-			createDataColumn({
-				header: 'Age',
-				key: 'age',
-			}),
-		]);
+	describe('data shape', () => {
+		test('no footers', () => {
+			const columns = createColumns<SampleRow>([
+				createDataColumn({
+					header: 'First Name',
+					key: 'firstName',
+				}),
+				createDataColumn({
+					header: 'Last Name',
+					key: 'lastName',
+				}),
+				createDataColumn({
+					header: 'Age',
+					key: 'age',
+				}),
+			]);
 
-		const actual = getFooterRows(columns);
+			const actual = getFooterRows(columns);
 
-		const expected: FooterRow<SampleRow>[] = [];
+			const expected: FooterRow<SampleRow>[] = [];
 
-		expect(actual).toStrictEqual(expected);
+			expect(actual).toStrictEqual(expected);
+		});
+
+		test('no footers on level 0', () => {
+			const columns = createColumns<SampleRow>([
+				createGroup({
+					header: 'Info',
+					columns: [
+						createDataColumn({
+							header: 'First Name',
+							footer: 'First Name',
+							key: 'firstName',
+						}),
+						createDataColumn({
+							header: 'Last Name',
+							footer: 'Last Name',
+							key: 'lastName',
+						}),
+						createDataColumn({
+							header: 'Age',
+							footer: 'Age',
+							key: 'age',
+						}),
+					],
+				}),
+			]);
+
+			const actual = getFooterRows(columns);
+
+			const expected: FooterRow<SampleRow>[] = [
+				new FooterRow({
+					cells: [
+						new FooterDataCell({
+							type: 'data',
+							label: 'First Name',
+							key: 'firstName',
+							colspan: 1,
+						}),
+						new FooterDataCell({
+							type: 'data',
+							label: 'Last Name',
+							key: 'lastName',
+							colspan: 1,
+						}),
+						new FooterDataCell({
+							type: 'data',
+							label: 'Age',
+							key: 'age',
+							colspan: 1,
+						}),
+					],
+				}),
+			];
+
+			expect(actual).toStrictEqual(expected);
+		});
+
+		test('no footers on level 1', () => {
+			const columns = createColumns<SampleRow>([
+				createGroup({
+					header: 'Info',
+					footer: 'Info',
+					columns: [
+						createDataColumn({
+							header: 'First Name',
+							key: 'firstName',
+						}),
+						createDataColumn({
+							header: 'Last Name',
+							key: 'lastName',
+						}),
+						createDataColumn({
+							header: 'Age',
+							key: 'age',
+						}),
+					],
+				}),
+			]);
+
+			const actual = getFooterRows(columns);
+
+			const expected: FooterRow<SampleRow>[] = [
+				new FooterRow({
+					cells: [
+						new FooterGroupCell({
+							type: 'group',
+							colspan: 3,
+							label: 'Info',
+						}),
+					],
+				}),
+			];
+
+			expect(actual).toStrictEqual(expected);
+		});
+
+		test('3 columns', () => {
+			const columns = createColumns<SampleRow>([
+				createDataColumn({
+					header: 'First Name',
+					footer: 'First Name',
+					key: 'firstName',
+				}),
+				createDataColumn({
+					header: 'Last Name',
+					footer: 'Last Name',
+					key: 'lastName',
+				}),
+				createDataColumn({
+					header: 'Age',
+					footer: 'Age',
+					key: 'age',
+				}),
+			]);
+
+			const actual = getFooterRows(columns);
+
+			const expected: FooterRow<SampleRow>[] = [
+				new FooterRow({
+					cells: [
+						new FooterDataCell({
+							type: 'data',
+							label: 'First Name',
+							key: 'firstName',
+							colspan: 1,
+						}),
+						new FooterDataCell({
+							type: 'data',
+							label: 'Last Name',
+							key: 'lastName',
+							colspan: 1,
+						}),
+						new FooterDataCell({
+							type: 'data',
+							label: 'Age',
+							key: 'age',
+							colspan: 1,
+						}),
+					],
+				}),
+			];
+
+			expect(actual).toStrictEqual(expected);
+		});
+
+		test('1 group column over 3 columns', () => {
+			const columns = createColumns<SampleRow>([
+				createGroup({
+					header: 'Info',
+					footer: 'Info',
+					columns: [
+						createDataColumn({
+							header: 'First Name',
+							footer: 'First Name',
+							key: 'firstName',
+						}),
+						createDataColumn({
+							header: 'Last Name',
+							footer: 'Last Name',
+							key: 'lastName',
+						}),
+						createDataColumn({
+							header: 'Age',
+							footer: 'Age',
+							key: 'age',
+						}),
+					],
+				}),
+			]);
+
+			const actual = getFooterRows(columns);
+
+			const expected: FooterRow<SampleRow>[] = [
+				new FooterRow({
+					cells: [
+						new FooterDataCell({
+							type: 'data',
+							label: 'First Name',
+							key: 'firstName',
+							colspan: 1,
+						}),
+						new FooterDataCell({
+							type: 'data',
+							label: 'Last Name',
+							key: 'lastName',
+							colspan: 1,
+						}),
+						new FooterDataCell({
+							type: 'data',
+							label: 'Age',
+							key: 'age',
+							colspan: 1,
+						}),
+					],
+				}),
+				new FooterRow({
+					cells: [
+						new FooterGroupCell({
+							type: 'group',
+							colspan: 3,
+							label: 'Info',
+						}),
+					],
+				}),
+			];
+
+			expect(actual).toStrictEqual(expected);
+		});
+
+		test('1 group column over 2 columns, 1 group column over 3 columns', () => {
+			const columns = createColumns<SampleRow>([
+				createGroup({
+					header: 'Name',
+					footer: 'Name',
+					columns: [
+						createDataColumn({
+							header: 'First Name',
+							footer: 'First Name',
+							key: 'firstName',
+						}),
+						createDataColumn({
+							header: 'Last Name',
+							footer: 'Last Name',
+							key: 'lastName',
+						}),
+					],
+				}),
+				createGroup({
+					header: 'Info',
+					footer: 'Info',
+					columns: [
+						createDataColumn({
+							header: 'Age',
+							footer: 'Age',
+							key: 'age',
+						}),
+						createDataColumn({
+							header: 'Status',
+							footer: 'Status',
+							key: 'status',
+						}),
+						createDataColumn({
+							header: 'Profile Progress',
+							footer: 'Profile Progress',
+							key: 'progress',
+						}),
+					],
+				}),
+			]);
+
+			const actual = getFooterRows(columns);
+
+			const expected: FooterRow<SampleRow>[] = [
+				new FooterRow({
+					cells: [
+						new FooterDataCell({
+							type: 'data',
+							label: 'First Name',
+							key: 'firstName',
+							colspan: 1,
+						}),
+						new FooterDataCell({
+							type: 'data',
+							label: 'Last Name',
+							key: 'lastName',
+							colspan: 1,
+						}),
+						new FooterDataCell({
+							type: 'data',
+							label: 'Age',
+							key: 'age',
+							colspan: 1,
+						}),
+						new FooterDataCell({
+							type: 'data',
+							label: 'Status',
+							key: 'status',
+							colspan: 1,
+						}),
+						new FooterDataCell({
+							type: 'data',
+							label: 'Profile Progress',
+							key: 'progress',
+							colspan: 1,
+						}),
+					],
+				}),
+				new FooterRow({
+					cells: [
+						new FooterGroupCell({
+							type: 'group',
+							colspan: 2,
+							label: 'Name',
+						}),
+						new FooterGroupCell({
+							type: 'group',
+							colspan: 3,
+							label: 'Info',
+						}),
+					],
+				}),
+			];
+
+			expect(actual).toStrictEqual(expected);
+		});
+
+		test('uneven columns, 1 group column over 2 columns, 3 columns', () => {
+			const columns = createColumns<SampleRow>([
+				createGroup({
+					header: 'Name',
+					footer: 'Name',
+					columns: [
+						createDataColumn({
+							header: 'First Name',
+							footer: 'First Name',
+							key: 'firstName',
+						}),
+						createDataColumn({
+							header: 'Last Name',
+							footer: 'Last Name',
+							key: 'lastName',
+						}),
+					],
+				}),
+				createDataColumn({
+					header: 'Age',
+					footer: 'Age',
+					key: 'age',
+				}),
+				createDataColumn({
+					header: 'Status',
+					footer: 'Status',
+					key: 'status',
+				}),
+				createDataColumn({
+					header: 'Profile Progress',
+					footer: 'Profile Progress',
+					key: 'progress',
+				}),
+			]);
+
+			const actual = getFooterRows(columns);
+
+			const expected: FooterRow<SampleRow>[] = [
+				new FooterRow({
+					cells: [
+						new FooterDataCell({
+							type: 'data',
+							label: 'First Name',
+							key: 'firstName',
+							colspan: 1,
+						}),
+						new FooterDataCell({
+							type: 'data',
+							label: 'Last Name',
+							key: 'lastName',
+							colspan: 1,
+						}),
+						new FooterDataCell({
+							type: 'data',
+							label: 'Age',
+							key: 'age',
+							colspan: 1,
+						}),
+						new FooterDataCell({
+							type: 'data',
+							label: 'Status',
+							key: 'status',
+							colspan: 1,
+						}),
+						new FooterDataCell({
+							type: 'data',
+							label: 'Profile Progress',
+							key: 'progress',
+							colspan: 1,
+						}),
+					],
+				}),
+				new FooterRow({
+					cells: [
+						new FooterGroupCell({
+							type: 'group',
+							colspan: 2,
+							label: 'Name',
+						}),
+						FOOTER_BLANK,
+						FOOTER_BLANK,
+						FOOTER_BLANK,
+					],
+				}),
+			];
+
+			expect(actual).toStrictEqual(expected);
+		});
 	});
 
-	test('no footers on level 0', () => {
-		const columns = createColumns<SampleRow>([
-			createGroup({
-				header: 'Info',
-				columns: [
-					createDataColumn({
-						header: 'First Name',
-						footer: 'First Name',
-						key: 'firstName',
-					}),
-					createDataColumn({
-						header: 'Last Name',
-						footer: 'Last Name',
-						key: 'lastName',
-					}),
-					createDataColumn({
-						header: 'Age',
-						footer: 'Age',
-						key: 'age',
-					}),
-				],
-			}),
-		]);
+	describe('data prototype', () => {
+		it('creates instances of FooterRow', () => {
+			const columns = createColumns<SampleRow>([
+				createDataColumn({
+					header: 'First Name',
+					footer: 'First Name',
+					key: 'firstName',
+				}),
+				createDataColumn({
+					header: 'Last Name',
+					footer: 'Last Name',
+					key: 'lastName',
+				}),
+				createDataColumn({
+					header: 'Age',
+					footer: 'Age',
+					key: 'age',
+				}),
+			]);
 
-		const actual = getFooterRows(columns);
+			const actual = getFooterRows(columns);
 
-		const expected: FooterRow<SampleRow>[] = [
-			new FooterRow({
-				cells: [
-					new FooterDataCell({
-						type: 'data',
-						label: 'First Name',
-						key: 'firstName',
-						colspan: 1,
-					}),
-					new FooterDataCell({
-						type: 'data',
-						label: 'Last Name',
-						key: 'lastName',
-						colspan: 1,
-					}),
-					new FooterDataCell({
-						type: 'data',
-						label: 'Age',
-						key: 'age',
-						colspan: 1,
-					}),
-				],
-			}),
-		];
+			actual.forEach((row) => {
+				expect(row).toBeInstanceOf(FooterRow);
+			});
+		});
 
-		expect(actual).toStrictEqual(expected);
-	});
+		it('creates instances of FooterGroupCell and FooterDataCell', () => {
+			const columns = createColumns<SampleRow>([
+				createGroup({
+					header: 'Info',
+					footer: 'Info',
+					columns: [
+						createDataColumn({
+							header: 'First Name',
+							footer: 'First Name',
+							key: 'firstName',
+						}),
+						createDataColumn({
+							header: 'Last Name',
+							footer: 'Last Name',
+							key: 'lastName',
+						}),
+						createDataColumn({
+							header: 'Age',
+							footer: 'Age',
+							key: 'age',
+						}),
+					],
+				}),
+			]);
 
-	test('no footers on level 1', () => {
-		const columns = createColumns<SampleRow>([
-			createGroup({
-				header: 'Info',
-				footer: 'Info',
-				columns: [
-					createDataColumn({
-						header: 'First Name',
-						key: 'firstName',
-					}),
-					createDataColumn({
-						header: 'Last Name',
-						key: 'lastName',
-					}),
-					createDataColumn({
-						header: 'Age',
-						key: 'age',
-					}),
-				],
-			}),
-		]);
+			const actual = getFooterRows(columns);
 
-		const actual = getFooterRows(columns);
-
-		const expected: FooterRow<SampleRow>[] = [
-			new FooterRow({
-				cells: [
-					new FooterGroupCell({
-						type: 'group',
-						colspan: 3,
-						label: 'Info',
-					}),
-				],
-			}),
-		];
-
-		expect(actual).toStrictEqual(expected);
-	});
-
-	test('3 columns', () => {
-		const columns = createColumns<SampleRow>([
-			createDataColumn({
-				header: 'First Name',
-				footer: 'First Name',
-				key: 'firstName',
-			}),
-			createDataColumn({
-				header: 'Last Name',
-				footer: 'Last Name',
-				key: 'lastName',
-			}),
-			createDataColumn({
-				header: 'Age',
-				footer: 'Age',
-				key: 'age',
-			}),
-		]);
-
-		const actual = getFooterRows(columns);
-
-		const expected: FooterRow<SampleRow>[] = [
-			new FooterRow({
-				cells: [
-					new FooterDataCell({
-						type: 'data',
-						label: 'First Name',
-						key: 'firstName',
-						colspan: 1,
-					}),
-					new FooterDataCell({
-						type: 'data',
-						label: 'Last Name',
-						key: 'lastName',
-						colspan: 1,
-					}),
-					new FooterDataCell({
-						type: 'data',
-						label: 'Age',
-						key: 'age',
-						colspan: 1,
-					}),
-				],
-			}),
-		];
-
-		expect(actual).toStrictEqual(expected);
-	});
-
-	test('1 group column over 3 columns', () => {
-		const columns = createColumns<SampleRow>([
-			createGroup({
-				header: 'Info',
-				footer: 'Info',
-				columns: [
-					createDataColumn({
-						header: 'First Name',
-						footer: 'First Name',
-						key: 'firstName',
-					}),
-					createDataColumn({
-						header: 'Last Name',
-						footer: 'Last Name',
-						key: 'lastName',
-					}),
-					createDataColumn({
-						header: 'Age',
-						footer: 'Age',
-						key: 'age',
-					}),
-				],
-			}),
-		]);
-
-		const actual = getFooterRows(columns);
-
-		const expected: FooterRow<SampleRow>[] = [
-			new FooterRow({
-				cells: [
-					new FooterDataCell({
-						type: 'data',
-						label: 'First Name',
-						key: 'firstName',
-						colspan: 1,
-					}),
-					new FooterDataCell({
-						type: 'data',
-						label: 'Last Name',
-						key: 'lastName',
-						colspan: 1,
-					}),
-					new FooterDataCell({
-						type: 'data',
-						label: 'Age',
-						key: 'age',
-						colspan: 1,
-					}),
-				],
-			}),
-			new FooterRow({
-				cells: [
-					new FooterGroupCell({
-						type: 'group',
-						colspan: 3,
-						label: 'Info',
-					}),
-				],
-			}),
-		];
-
-		expect(actual).toStrictEqual(expected);
-	});
-
-	test('1 group column over 2 columns, 1 group column over 3 columns', () => {
-		const columns = createColumns<SampleRow>([
-			createGroup({
-				header: 'Name',
-				footer: 'Name',
-				columns: [
-					createDataColumn({
-						header: 'First Name',
-						footer: 'First Name',
-						key: 'firstName',
-					}),
-					createDataColumn({
-						header: 'Last Name',
-						footer: 'Last Name',
-						key: 'lastName',
-					}),
-				],
-			}),
-			createGroup({
-				header: 'Info',
-				footer: 'Info',
-				columns: [
-					createDataColumn({
-						header: 'Age',
-						footer: 'Age',
-						key: 'age',
-					}),
-					createDataColumn({
-						header: 'Status',
-						footer: 'Status',
-						key: 'status',
-					}),
-					createDataColumn({
-						header: 'Profile Progress',
-						footer: 'Profile Progress',
-						key: 'progress',
-					}),
-				],
-			}),
-		]);
-
-		const actual = getFooterRows(columns);
-
-		const expected: FooterRow<SampleRow>[] = [
-			new FooterRow({
-				cells: [
-					new FooterDataCell({
-						type: 'data',
-						label: 'First Name',
-						key: 'firstName',
-						colspan: 1,
-					}),
-					new FooterDataCell({
-						type: 'data',
-						label: 'Last Name',
-						key: 'lastName',
-						colspan: 1,
-					}),
-					new FooterDataCell({
-						type: 'data',
-						label: 'Age',
-						key: 'age',
-						colspan: 1,
-					}),
-					new FooterDataCell({
-						type: 'data',
-						label: 'Status',
-						key: 'status',
-						colspan: 1,
-					}),
-					new FooterDataCell({
-						type: 'data',
-						label: 'Profile Progress',
-						key: 'progress',
-						colspan: 1,
-					}),
-				],
-			}),
-			new FooterRow({
-				cells: [
-					new FooterGroupCell({
-						type: 'group',
-						colspan: 2,
-						label: 'Name',
-					}),
-					new FooterGroupCell({
-						type: 'group',
-						colspan: 3,
-						label: 'Info',
-					}),
-				],
-			}),
-		];
-
-		expect(actual).toStrictEqual(expected);
-	});
-
-	test('uneven columns, 1 group column over 2 columns, 3 columns', () => {
-		const columns = createColumns<SampleRow>([
-			createGroup({
-				header: 'Name',
-				footer: 'Name',
-				columns: [
-					createDataColumn({
-						header: 'First Name',
-						footer: 'First Name',
-						key: 'firstName',
-					}),
-					createDataColumn({
-						header: 'Last Name',
-						footer: 'Last Name',
-						key: 'lastName',
-					}),
-				],
-			}),
-			createDataColumn({
-				header: 'Age',
-				footer: 'Age',
-				key: 'age',
-			}),
-			createDataColumn({
-				header: 'Status',
-				footer: 'Status',
-				key: 'status',
-			}),
-			createDataColumn({
-				header: 'Profile Progress',
-				footer: 'Profile Progress',
-				key: 'progress',
-			}),
-		]);
-
-		const actual = getFooterRows(columns);
-
-		const expected: FooterRow<SampleRow>[] = [
-			new FooterRow({
-				cells: [
-					new FooterDataCell({
-						type: 'data',
-						label: 'First Name',
-						key: 'firstName',
-						colspan: 1,
-					}),
-					new FooterDataCell({
-						type: 'data',
-						label: 'Last Name',
-						key: 'lastName',
-						colspan: 1,
-					}),
-					new FooterDataCell({
-						type: 'data',
-						label: 'Age',
-						key: 'age',
-						colspan: 1,
-					}),
-					new FooterDataCell({
-						type: 'data',
-						label: 'Status',
-						key: 'status',
-						colspan: 1,
-					}),
-					new FooterDataCell({
-						type: 'data',
-						label: 'Profile Progress',
-						key: 'progress',
-						colspan: 1,
-					}),
-				],
-			}),
-			new FooterRow({
-				cells: [
-					new FooterGroupCell({
-						type: 'group',
-						colspan: 2,
-						label: 'Name',
-					}),
-					FOOTER_BLANK,
-					FOOTER_BLANK,
-					FOOTER_BLANK,
-				],
-			}),
-		];
-
-		expect(actual).toStrictEqual(expected);
+			actual[0].cells.forEach((cell) => {
+				expect(cell).toBeInstanceOf(FooterDataCell);
+			});
+			actual[1].cells.forEach((cell) => {
+				expect(cell).toBeInstanceOf(FooterGroupCell);
+			});
+		});
 	});
 });
