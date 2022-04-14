45babdbe5305ba3482bf9f555eac96f0fc0ba630
Bryan Lee <bryanmylee@gmail.com>
2022-04-14 14:43:01 +0800

    refactor: tableInstance

--- src/lib/components/Table.svelte
+++ src/lib/components/Table.svelte
@@ -1,19 +1,12 @@
 <script lang="ts">
 	import type { Column } from '$lib/models/Column';
-	import { getDataColumns } from '$lib/utils/getDataColumns';
-	import { getDataRows } from '$lib/utils/getDataRows';
-	import { getFooterRows } from '$lib/utils/getFooterRows';
-	import { getHeaderRows } from '$lib/utils/getHeaderRows';
+	import { TableInstance } from '$lib/models/TableInstance';
 
 	type Item = $$Generic<object>;
 
-	export let columns: Column<Item>[];
-	const dataColumns = getDataColumns(columns);
-	const headerRows = getHeaderRows(columns);
-	const footerRows = getFooterRows(columns);
-
 	export let data: Item[];
-	const dataRows = getDataRows(data, dataColumns);
+	export let columns: Column<Item>[];
+	const tableInstance = new TableInstance({ data, columns });
 </script>
 
-<slot {data} {dataColumns} {headerRows} {footerRows} {dataRows} />
+<slot {...tableInstance} />
---
+++ src/lib/models/TableInstance.ts
@@ -0,0 +1,29 @@
+import { getDataColumns } from '$lib/utils/getDataColumns';
+import { getDataRows } from '$lib/utils/getDataRows';
+import { getFooterRows } from '$lib/utils/getFooterRows';
+import { getHeaderRows } from '$lib/utils/getHeaderRows';
+import type { Column, DataColumn } from './Column';
+import type { DataRow } from './DataRow';
+import type { FooterRow } from './FooterRow';
+import type { HeaderRow } from './HeaderRow';
+
+export interface TableInstanceInit<Item extends object> {
+	columns: Column<Item>[];
+	data: Item[];
+}
+
+export class TableInstance<Item extends object> implements TableInstanceInit<Item> {
+	columns!: Column<Item>[];
+	data!: Item[];
+	dataColumns: DataColumn<Item>[];
+	headerRows: HeaderRow<Item>[];
+	footerRows: FooterRow<Item>[];
+	dataRows: DataRow<Item>[];
+	constructor({ columns, data }: TableInstanceInit<Item>) {
+		Object.assign(this, { columns, data });
+		this.dataColumns = getDataColumns(columns);
+		this.headerRows = getHeaderRows(columns);
+		this.footerRows = getFooterRows(columns);
+		this.dataRows = getDataRows(data, this.dataColumns);
+	}
+}
