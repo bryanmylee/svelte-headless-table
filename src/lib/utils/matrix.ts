528db3a3a4f77dfd40b580061ee5b4240a046670
Bryan Lee <bryanmylee@gmail.com>
2022-04-26 02:32:15 +0800

    feat: matrix utils

---
+++ src/lib/utils/matrix.ts
@@ -0,0 +1,26 @@
+import type { Matrix } from '$lib/types/Matrix';
+
+export const getNullMatrix = (width: number, height: number): Matrix<null> => {
+	const result: Matrix<null> = [];
+	// Use a loop to create a new array instance per row.
+	for (let i = 0; i < height; i++) {
+		result.push(Array(width).fill(null));
+	}
+	return result;
+};
+
+export const getTransposed = <T>(matrix: Matrix<T>): Matrix<T> => {
+	const height = matrix.length;
+	if (height === 0) {
+		return matrix;
+	}
+	const width = matrix[0].length;
+	const result: Matrix<T | null> = getNullMatrix(height, width);
+	for (let i = 0; i < height; i++) {
+		for (let j = 0; j < width; j++) {
+			result[i][j] = matrix[j][i];
+		}
+	}
+	// We guarantee that all elements are filled.
+	return result as Matrix<T>;
+};
