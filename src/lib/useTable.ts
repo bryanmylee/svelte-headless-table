45c7e1f7deacf8b26d4771cbd6037a66aec5894a
Bryan Lee <bryanmylee@gmail.com>
2022-04-26 04:18:49 +0800

    feat: derive reactive headerRows

--- src/lib/useTable.ts
+++ src/lib/useTable.ts
@@ -1,10 +1,24 @@
+import { derived, readable, type Readable } from 'svelte/store';
 import type { Column } from './columns';
 import { getHeaderRows } from './headerRows';
 
 export interface UseTableProps<Item> {
 	columns: Array<Column<Item>>;
+	columnOrder?: Readable<Array<string> | undefined>;
+	hiddenColumns?: Readable<Array<string> | undefined>;
 }
 
-export const useTable = <Item>({ columns }: UseTableProps<Item>) => {
-	const headerRows = getHeaderRows(columns);
+const Undefined = readable(undefined);
+
+export const useTable = <Item>({
+	columns,
+	columnOrder = Undefined,
+	hiddenColumns = Undefined,
+}: UseTableProps<Item>) => {
+	const headerRows = derived([columnOrder, hiddenColumns], ([$columnOrder, $hiddenColumns]) => {
+		return getHeaderRows(columns, {
+			columnOrder: $columnOrder,
+			hiddenColumns: $hiddenColumns,
+		});
+	});
 };
