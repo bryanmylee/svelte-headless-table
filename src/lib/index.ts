66a38556b16313186c63a2daa4c3132bc148c6fd
Bryan Lee <bryanmylee@gmail.com>
2022-05-06 15:40:45 +0800

    build: use npm

---
+++ src/lib/index.ts
@@ -0,0 +1,9 @@
+// components
+export { default as Render } from '$lib/components/Render.svelte';
+export { Subscribe } from 'svelte-subscribe';
+// plugins
+export * from './plugins';
+// table core
+export { createTable } from '$lib/createTable';
+export { useTable } from '$lib/useTable';
+export { createRender, type RenderConfig } from '$lib/render';
---
+++ src/lib/plugins/index.ts
@@ -0,0 +1,11 @@
+export {
+	useColumnFilters,
+	matchFilter,
+	numberRangeFilter,
+	textPrefixFilter,
+	type ColumnFilterFn,
+	type ColumnFilterFnProps,
+} from './useColumnFilters';
+export { useColumnOrder } from './useColumnOrder';
+export { useHiddenColumns } from './useHiddenColumns';
+export { useSortBy } from './useSortBy';
