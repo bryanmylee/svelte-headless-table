49671cab8baebe81d35554f468cfb182ead2b674
Bryan Lee <bryanmylee@gmail.com>
2022-05-21 23:35:01 +0800

    feat: store sets

--- src/lib/utils/store.ts
+++ src/lib/utils/store.ts
@@ -1,4 +1,4 @@
-import { readable, type Readable, type Writable } from 'svelte/store';
+import { readable, writable, type Readable, type Writable } from 'svelte/store';
 
 export type ReadOrWritable<T> = Readable<T> | Writable<T>;
 
@@ -26,3 +26,109 @@ export type ReadOrWritableKeys<T> = {
 
 export const Undefined = readable(undefined);
 export const UndefinedAs = <T>() => Undefined as unknown as Readable<T>;
+
+export interface ArraySetStoreOptions<T> {
+	isEqual?: (a: T, b: T) => boolean;
+}
+
+export interface ArraySetStore<T> extends Writable<T[]> {
+	toggle: (item: T) => void;
+	add: (item: T) => void;
+	remove: (item: T) => void;
+	reset: () => void;
+}
+
+export const arraySetStore = <T>(
+	initial: T[] = [],
+	{ isEqual = (a, b) => a === b }: ArraySetStoreOptions<T> = {}
+): ArraySetStore<T> => {
+	const { subscribe, update, set } = writable(initial);
+	const toggle = (item: T) => {
+		update(($arraySet) => {
+			const index = $arraySet.findIndex(($item) => isEqual($item, item));
+			if (index === -1) {
+				return [...$arraySet, item];
+			}
+			return [...$arraySet.slice(0, index), ...$arraySet.slice(index + 1)];
+		});
+	};
+	const add = (item: T) => {
+		update(($arraySet) => {
+			const index = $arraySet.findIndex(($item) => isEqual($item, item));
+			if (index === -1) {
+				return [...$arraySet, item];
+			}
+			return $arraySet;
+		});
+	};
+	const remove = (item: T) => {
+		update(($arraySet) => {
+			const index = $arraySet.findIndex(($item) => isEqual($item, item));
+			if (index === -1) {
+				return $arraySet;
+			}
+			return [...$arraySet.slice(0, index), ...$arraySet.slice(index + 1)];
+		});
+	};
+	const reset = () => {
+		set([]);
+	};
+	return {
+		subscribe,
+		update,
+		set,
+		toggle,
+		add,
+		remove,
+		reset,
+	};
+};
+
+export interface RecordSetStore<T extends string | number> extends Writable<Record<T, boolean>> {
+	toggle: (item: T) => void;
+	add: (item: T) => void;
+	remove: (item: T) => void;
+	reset: () => void;
+}
+
+export const recordSetStore = <T extends string | number>(
+	initial: Record<T, boolean> = {} as Record<T, boolean>
+): RecordSetStore<T> => {
+	const { subscribe, update, set } = writable(initial);
+	const toggle = (item: T) => {
+		update(($recordSet) => {
+			if ($recordSet[item] === true) {
+				delete $recordSet[item];
+				return $recordSet;
+			}
+			return {
+				...$recordSet,
+				[item]: true,
+			};
+		});
+	};
+	const add = (item: T) => {
+		update(($recordSet) => ({
+			...$recordSet,
+			[item]: true,
+		}));
+	};
+	const remove = (item: T) => {
+		update(($recordSet) => {
+			delete $recordSet[item];
+			return $recordSet;
+		});
+	};
+	const reset = () => {
+		set({} as Record<T, boolean>);
+	};
+	return {
+		subscribe,
+		update,
+		set,
+		toggle,
+		add,
+		remove,
+		reset,
+	};
+};
