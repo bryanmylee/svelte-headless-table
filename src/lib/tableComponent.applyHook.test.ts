cb57bd980cb717b0480e1c84b7d10280fa299c1f
Bryan Lee <bryanmylee@fb.com>
2022-05-26 15:47:34 +0800

    test: plugin attrs hook

--- src/lib/tableComponent.applyHook.test.ts
+++ src/lib/tableComponent.applyHook.test.ts
@@ -28,10 +28,79 @@ it('hooks plugin props', () => {
 	expect(get(actual)).toStrictEqual(expected);
 });
 
-it('throws if attrs is accessed without implementation', () => {
+it('hooks plugin attrs', () => {
 	const component = new TestComponent({ id: '0' });
+	const $attrs = {
+		a: 1,
+		b: 2,
+	};
+	const attrs = readable($attrs);
+	component.applyHook('test', { attrs });
 
-	expect(() => {
-		component.attrs();
-	}).toThrowError('Missing `attrs` implementation');
+	const actual = component.attrs();
+
+	const expected = $attrs;
+
+	expect(get(actual)).toStrictEqual(expected);
+});
+
+it('hooks and merges plugin attrs', () => {
+	const component = new TestComponent({ id: '0' });
+	const $attrs1 = {
+		a: 1,
+		b: 2,
+	};
+	const attrs1 = readable($attrs1);
+	const $attrs2 = {
+		c: 3,
+		b: 4,
+	};
+	const attrs2 = readable($attrs2);
+	component.applyHook('firstPlugin', { attrs: attrs1 });
+	component.applyHook('secondPlugin', { attrs: attrs2 });
+
+	const actual = component.attrs();
+
+	const expected = {
+		a: 1,
+		c: 3,
+		b: 4,
+	};
+
+	expect(get(actual)).toStrictEqual(expected);
+});
+
+it('hooks and merges plugin attrs styles', () => {
+	const component = new TestComponent({ id: '0' });
+	const $attrs1 = {
+		a: 1,
+		b: 2,
+		style: {
+			x: '1',
+			y: '2',
+		},
+	};
+	const attrs1 = readable($attrs1);
+	const $attrs2 = {
+		c: 3,
+		b: 4,
+		style: {
+			z: '3',
+			y: '4',
+		},
+	};
+	const attrs2 = readable($attrs2);
+	component.applyHook('firstPlugin', { attrs: attrs1 });
+	component.applyHook('secondPlugin', { attrs: attrs2 });
+
+	const actual = component.attrs();
+
+	const expected = {
+		a: 1,
+		b: 4,
+		c: 3,
+		style: 'x:1;y:4;z:3',
+	};
+
+	expect(get(actual)).toStrictEqual(expected);
 });
