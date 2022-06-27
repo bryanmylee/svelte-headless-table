6078dd0f8b8d2142cbf959811867eba6bb683e16
Bryan Lee <bryanmylee@fb.com>
2022-06-27 18:56:20 +0800

    chore: export all model types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,3 +4,16 @@ export { Subscribe } from 'svelte-subscribe';
 // table core
 export { createTable } from '$lib/createTable';
 export { createRender, type RenderConfig } from '$lib/render';
+// models
+export { HeaderRow } from '$lib/headerRows';
+export {
+	HeaderCell,
+	FlatHeaderCell,
+	DataHeaderCell,
+	FlatDisplayHeaderCell,
+	GroupHeaderCell,
+	GroupDisplayHeaderCell,
+} from '$lib/headerCells';
+export { BodyRow, DisplayBodyRow, DataBodyRow } from '$lib/bodyRows';
+export { BodyCell, DataBodyCell, DisplayBodyCell } from '$lib/bodyCells';
+export type { TableViewModel, TableState } from '$lib/createViewModel';
