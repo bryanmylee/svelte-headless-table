6c8d73724001f7c7f22e0e898680caa83015ec97
Bryan Lee <bryanmylee@fb.com>
2022-07-29 22:49:15 +0800

    chore: export Label classes

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -19,3 +19,4 @@ export { BodyRow, DisplayBodyRow, DataBodyRow } from '$lib/bodyRows';
 export { BodyCell, DataBodyCell, DisplayBodyCell } from '$lib/bodyCells';
 export { Column, FlatColumn, DataColumn, GroupColumn, DisplayColumn } from '$lib/columns';
 export type { TableViewModel, TableState } from '$lib/createViewModel';
+export type { DataLabel, DisplayLabel, HeaderLabel } from '$lib/types/Label';
