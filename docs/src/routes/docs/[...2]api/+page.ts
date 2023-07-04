fa6fe1b7d1da618240638652e4ba1d33d9a7aff0
Bryan Lee <bryanmylee@gmail.com>
2023-07-05 03:44:38 +0800

    doc(fix): remove pre-renderable hint

--- docs/src/routes/docs/[...2]api/+page.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export const prerender = true;
-
-/** @type {import('@sveltejs/kit').PageLoad} */
-export async function load() {
-  throw redirect(307, `/docs/api/create-table`);
-}
