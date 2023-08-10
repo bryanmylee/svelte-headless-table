3b9825e431c5b1fb1dd9596f1ab8cf147e833d91
Bryan Lee <bryanmylee@gmail.com>
2023-08-11 02:40:04 +0800

    fix: src/ import

--- src/lib/plugins/addGroupBy.test.ts
+++ src/lib/plugins/addGroupBy.test.ts
@@ -1,5 +1,5 @@
 import { createTable } from '../createTable';
-import type { Sample } from 'src/routes/_createSamples';
+import type { Sample } from '../../routes/_createSamples';
 import { get, readable } from 'svelte/store';
 import { addGroupBy } from './addGroupBy';
 import { addSubRows } from './addSubRows';
